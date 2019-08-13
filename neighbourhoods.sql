729eee3ba9a6ee1f82900a83b07fc46e949bc5f8
MatthieuvdSlikke <46849165+MatthieuvdSlikke@users.noreply.github.com>
2019-08-13 12:00:15 +0200

    Add files via upload

---
+++ neighbourhoods.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE Neighbourhoods
+(
+  area_id integer,
+  area_name character varying,
+  land_area numeric,
+  population integer,
+  number_of_dwellings integer,
+  number_of_businesses integer
+)
---
+++ spatial_join.sql
@@ -0,0 +1,4 @@
+﻿create table spatial_join as (
+select N.area_name, sum(num_cars)
+from "Neighbourhoods_location" N left join "CarSharingPods" C on ((latitude between northeast_lat and southwest_lat) and (longitude between northeast_lng and southwest_lat ) )
+group by N.area_name)
